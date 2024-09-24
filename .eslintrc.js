baff8120131373e7d6582d973b18b74c35d07683
Jisu <hellojs242@gmail.com>
2024-09-24 23:21:08 +0900

    :hammer: chore: Eslint rule 설정
    - import 정렬
    - any 사용, return type 부재 시 경고

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,48 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
+
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '**/*spec.ts'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      { allowExpressions: true },
+    ], // 함수의 반환 타입 명시
+    '@typescript-eslint/no-explicit-any': 'warn', // any 사용시 경고
+    'import/order': [
+      // import 순서 정렬
+      'error',
+      {
+        alphabetize: {
+          order: 'asc', //alphaber 순서로 정렬
+        },
+        'newlines-between': 'always', // 그룹 별 newline 으로 구분
+        //그룹은 기본 ["builtin", "external", "parent", "sibling", "index"] 순서입니다. 변경 가능
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json', // tsconfig 경로 설정
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
