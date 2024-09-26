6bd7b3df11ac0e64288fa8ae6a55a789e9303d29
Jisu <hellojs242@gmail.com>
2024-09-26 21:57:50 +0900

    :sparkles: feat: prisma migration file 추가 #3

---
+++ prisma/migrations/20240926125316_init/migration.sql
@@ -0,0 +1,72 @@
+-- CreateTable
+CREATE TABLE "member" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" VARCHAR(255) NOT NULL,
+    "kakao_id" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "member_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "saved_book" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "member_id" UUID NOT NULL,
+    "book_id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "saved_book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "book" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "title" VARCHAR(255) NOT NULL,
+    "link" VARCHAR(2048) NOT NULL,
+    "image" VARCHAR(2048) NOT NULL,
+    "author" VARCHAR(255) NOT NULL,
+    "discount" INTEGER,
+    "publisher" VARCHAR(255) NOT NULL,
+    "isbn" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "pubdate" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "note" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "content" VARCHAR(1024) NOT NULL,
+    "book_id" UUID NOT NULL,
+    "member_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "note_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "saved_book_member_id_book_id_key" ON "saved_book"("member_id", "book_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "book_isbn_key" ON "book"("isbn");
+
+-- AddForeignKey
+ALTER TABLE "saved_book" ADD CONSTRAINT "saved_book_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "saved_book" ADD CONSTRAINT "saved_book_member_id_fkey" FOREIGN KEY ("member_id") REFERENCES "member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "note" ADD CONSTRAINT "note_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "note" ADD CONSTRAINT "note_member_id_fkey" FOREIGN KEY ("member_id") REFERENCES "member"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
